b2fa5800614fe23a5c241e1e6d659377a32616d9
Matt Eilar <meilar@gmail.com>
2022-02-25 10:18:22 -0800

    #red add pricing logic tests

--- Bakery.Tests/ModelTests/Pastry.Tests.cs
+++ Bakery.Tests/ModelTests/Pastry.Tests.cs
@@ -12,5 +12,17 @@ namespace Bakery.Tests
       Pastry newPastry = new Pastry(1);
       Assert.AreEqual(typeof(Pastry), newPastry.GetType());
     }
+    [TestMethod]
+    public void Constructor_RunsPricingLogic_OrderTotal()
+    {
+      Pastry pastry1 = new Pastry(1);
+      Assert.AreEqual(pastry1.Total, 2);
+      Pastry pastry2 = new Pastry(2);
+      Assert.AreEqual(pastry2.Total, 4);
+      Pastry pastry3 = new Pastry(3);
+      Assert.AreEqual(pastry3.Total, 5);
+      Pastry pastry7 = new Pastry(7);
+      Assert.AreEqual(pastry7.Total, 12);
+    }
   }
 }
