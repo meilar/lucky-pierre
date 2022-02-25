213382c8964f10b7d50fd687d75700959a194581
Matt Eilar <meilar@gmail.com>
2022-02-25 14:13:13 -0800

    Complete UI, MVP reached

--- Bakery/Models/Welcome.cs
+++ Bakery/Models/Welcome.cs
@@ -11,11 +11,73 @@ namespace Bakery.Models
 /_/   /_/\___/_/  /_/   \___/____/  /_____/\__,_/_/|_|\___/_/   \__, /  
                                                                /____/   
 
-    Version 1. 0 (c) 2022, Matt Eilar
+    Version 1. 0 (c) 2022 Matthew Eilar
 
     -----------------------------------------------------------------------
 
     Press ENTER to continue.
+    ";
+
+    public static string Intro = @"
+    Welcome to Pierre's Bakery. We sell baked goods, BREAD and PASTRIES.
+    Do you know what JAMES BEARD said about bread? 
+    
+    'Good bread is the most fundamentally satisfying of all foods; 
+    and good bread with fresh butter, the greatest of feasts.'
+
+    We have a YUKON HERITAGE DAY special this weekend.
+
+                  Bread                         Pastries
+                =========                     ===========
+                $5 per loaf                     $2 each
+                BUY 2 GET 1 FREE!               $5 for 3
+    
+    Would you like to buy any BREAD? Enter a number of loaves to purchase, or enter 0 to decline.
+    ";
+
+    public static string PastriesText = @"
+    We also have PASTRIES available. As ALAIN BREMOND-TORRENT once wrote:
+
+    'The softest pain on earth must be the pain au chocolat.'
+
+    Would you like to buy any PASTRIES? Enter a number of treats to purchase, or enter 0 to decline.
+    ";
+
+    public static string Outtro = @"
+    Oh, and don't forget, as MAHATMA GHANDI said:
+
+    'There are people in the world so hungry, that God cannot appear to them except in the form of bread.'
+
+    ";
+    
+    public static string Grafix = @"
+                   ______                ____               __
+                  / ____/___  ____  ____/ / /_  __  _____  / /
+                 / / __/ __ \/ __ \/ __  / __ \/ / / / _ \/ / 
+                / /_/ / /_/ / /_/ / /_/ / /_/ / /_/ /  __/_/  
+                \____/\____/\____/\__,_/_.___/\__, /\___(_)   
+                                            /____/                                                                  
+                                                                                        
+                                        ██████                                    
+                            ████████████░░░░░░██                                  
+                          ██░░░░░░░░░░░░██░░░░░░██████                            
+                        ██░░░░░░░░░░░░░░██░░░░░░██░░░░██                          
+                      ██░░░░░░░░░░░░░░░░░░██░░░░██░░░░░░██                        
+                      ██░░░░░░░░░░░░░░░░░░░░██░░██░░░░░░██                        
+                      ██░░░░░░░░░░░░░░░░░░░░██░░████████                          
+                    ██░░████░░░░░░░░░░░░░░░░██░░██                                
+                    ██░░░░░░████░░░░░░░░░░░░████                                  
+                  ██░░░░░░░░░░░░████░░░░░░░░██                                    
+                  ██░░░░░░░░░░░░░░░░██░░░░██                                      
+                    ██░░░░░░░░░░░░░░██████                                        
+                  ██░░████████░░░░░░██                                            
+                  ██░░░░░░░░░░██████                                              
+                    ██░░░░░░░░░░██                                                
+                  ██░░██████████                                                  
+                  ██░░░░░░░░██                                                    
+                    ██░░░░██                                                      
+                      ████         
+    
     ";
   }
 }
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using Bakery.Models;
 
 namespace Bakery
@@ -6,8 +7,60 @@ namespace Bakery
   public class Program
   {
     static void Main()
-  {
-    Console.WriteLine(Welcome.Banner);
+    {
+      Console.WriteLine(Welcome.Banner);
+      Console.ReadKey();
+      Console.WriteLine(Welcome.Intro);
+      int breadInput = Int32.Parse(Console.ReadLine());
+      Bread userBread = new Bread(breadInput);
+
+      if (breadInput == 0)
+      {
+        Console.WriteLine();
+        Console.WriteLine("Thank you.");
+      }
+      else
+      {
+        Console.WriteLine();
+        Console.WriteLine("Thank you. You have ordered " + userBread.Quantity + " loaf/loaves of bread, and your order total is now $" + userBread.Total);
+      }
+
+      Console.WriteLine(Welcome.PastriesText);
+      int pastryInput = Int32.Parse(Console.ReadLine());
+      Pastry userPastry = new Pastry(pastryInput);
+
+      if (pastryInput == 0 && breadInput == 0)
+      {
+        Console.WriteLine();
+        Console.WriteLine("OK, then. Thanks for stopping in, and maybe you'll try something another time.");
+        Console.WriteLine(Welcome.Outtro);
+        Thread.Sleep(5000);
+        Console.WriteLine(Welcome.Grafix);
+      }
+      else if (pastryInput == 0)
+      {
+        Console.WriteLine();
+        Console.WriteLine("Alrighty then. Your order stands at " + userBread.Quantity + " loaf/loaves of bread, and a grand order total of $" + userBread.Total + " Press ENTER to pay.");
+        Console.WriteLine();
+        Console.ReadKey();
+        Console.WriteLine("Your payment has been accepted. Thank you for shopping with us.");
+        Console.WriteLine(Welcome.Outtro);
+        Thread.Sleep(5000);
+        Console.WriteLine(Welcome.Grafix);
+      }
+      else
+      {
+        Console.WriteLine();
+        Console.WriteLine("Great. Your order is now " + userPastry.Quantity + " pastry/pastries and " + userBread.Quantity + " loaf/loaves of bread. You now have a grand order total of $" + (userBread.Total + userPastry.Total) + ". Press ENTER to pay.");
+        Console.ReadKey();
+        Console.WriteLine();
+        Console.WriteLine("Your payment has been accepted. Thank you for shopping with us.");
+        Console.WriteLine(Welcome.Outtro);
+        Thread.Sleep(10000);
+        Console.WriteLine(Welcome.Grafix);
+      }
+
+
+    }
   }
-}
 }
